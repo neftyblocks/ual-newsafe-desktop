6c81cf30c1a6ab052e16f55f0b0e3b98b8a7813e
Javier P <javierp@neftyblocks.com>
2023-03-09 15:26:05 +0100

    Initial commit

---
+++ build.config.ts
@@ -0,0 +1,18 @@
+import { defineBuildConfig } from 'unbuild';
+
+export default defineBuildConfig({
+    declaration: true,
+    rollup: {
+        emitCJS: true,
+    },
+    clean: true,
+    outDir: 'lib',
+    entries: ['src/index'],
+    externals: [
+        '@greymass/eosio',
+        'anchor-link',
+        'anchor-link-browser-transport',
+        'eosjs',
+        'universal-authenticator-library',
+    ],
+});
